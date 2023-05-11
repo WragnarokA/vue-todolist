e733dae02fe958486a7cabdb5a6dcf7a756a548f
WragnarokA <wilmer.arango.a@gmail.com>
2023-05-11 04:14:17 +0200

    fine

--- js/main.js
+++ js/main.js
@@ -3,19 +3,60 @@ const { createApp } = Vue
 createApp({
     data() {
         return{
-            list: [
-
-
+            taskList: [
+                { text: 'fare la spesa', done: false },
+                { text: 'montare la tv', done: false },
+                { text: 'fare rifornimento', done: false }
             ],
-   
-        }
-    }, 
-    methods: {
-        prevSlide() {
+            newTask:  {
+                text: "",
+                done: false,
+            },
+            error: false,
+                
+        }},             
+        methods: {
+            addTask() {
+                if (this.newTask.text.length > 3) {
+                    this.error = false;
+                    let nuovaTask = {...this.newTask}
+                    this.taskList.unshift(nuovaTask);
+                    this.newTask.text = "";  
+                } else {
+                    this.error = true;
+                }
+            }, 
+                    
+            removeTask(posizione){
+                this.taskList.splice(posizione, 1);
+            },
 
-             
-        }, 
-     } 
-}).mount('#app')
+            changeTask(posizione){
+                this.taskList[posizione].done = !this.taskList[posizione].done;
+            },
+         } 
+    }).mount('#app')
+                      
+
+                
+
+//     tuduList: [
+//         {food: [{
+//             text: "fare la spesa",
+//             done: false,
+//         }]},
+//         {tv: [{
+//             text: "montare la tv",
+//             done: false,
+//         }]},
+//         {car: [{
+//             text: "fare ",
+//             done: false,
+//         }]}
+        
+//     ],
+// }},     
+   
+    
 
             
